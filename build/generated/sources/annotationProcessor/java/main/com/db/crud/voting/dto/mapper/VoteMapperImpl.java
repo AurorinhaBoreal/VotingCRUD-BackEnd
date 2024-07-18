6d22d27e192ddd89ba2bd0fcd4d1c498c059f183
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-07-18 18:42:59 -0300

    Revert "US003 - User Flow"

---
+++ build/generated/sources/annotationProcessor/java/main/com/db/crud/voting/dto/mapper/VoteMapperImpl.java
@@ -0,0 +1,13 @@
+package com.db.crud.voting.dto.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-07-16T15:58:57-0300",
+    comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.8.jar, environment: Java 17.0.10 (Oracle Corporation)"
+)
+@Component
+public class VoteMapperImpl implements VoteMapper {
+}
